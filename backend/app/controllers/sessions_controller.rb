93d197b787d4ca29fcf803d6e5c7acd7faf294e9
yjkdev <dustmqgksmsdev24@gmail.com>
2025-05-12 14:00:53 +0900

    ✨ feat: login & sign page

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       token = JWT.encode({ user_id: user.id }, Rails.application.secrets.secret_key_base, 'HS256')
-      render json: { message: '로그인 성공', token: token }, status: :ok
+      render json: {
+        message: '로그인 성공',
+        token: token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }
+      }, status: :ok
     else
       render json: { error: '이메일 또는 비밀번호가 올바르지 않습니다' }, status: :unauthorized
     end
